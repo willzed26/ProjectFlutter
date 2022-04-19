615dfa21f954ede871ec3297c5cd4fa845efbc79
willzed <wiliamwijaya101@gmail.com>
2022-04-19 14:21:59 +0700

    add file

---
+++ android/app/src/main/kotlin/com/example/apk1/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.apk1
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(),
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text("first App"),
+        ),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [MyText(), MyText(), MyText()],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class MyText extends StatelessWidget {
+  const MyText({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Text("Hello WOrld");
+  }
+}
